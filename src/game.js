da7fa8faebb52d668ac5259d24be24f32158e17f
adnan wahab <hi@adnanwahab.com>
2017-05-31 21:25:52 -0400

    add the game

---
+++ src/game.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import _ from 'underscore';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <Game />,
+    document.body
+);
+
+
+export default App;
